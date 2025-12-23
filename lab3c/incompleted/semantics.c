c448203ff1609f513632a8ac7e91e459cec4cd91
chuthanh1310 <chuthanh1310@gmail.com>
2025-12-23 09:38:54 +0700

    lab3d

---
+++ lab3c/incompleted/semantics.c
@@ -0,0 +1,59 @@
+/* 
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "semantics.h"
+#include "error.h"
+
+extern SymTab* symtab;
+extern Token* currentToken;
+
+Object* lookupObject(char *name) {
+  Object *obj=NULL;
+  static Scope* scope=NULL;
+  scope = symtab->currentScope;
+  while (scope != NULL) {
+    obj = findObject(scope->objList, name);
+    if (obj != NULL)
+      return obj;
+    scope = scope->outer; 
+  }
+  return findObject(symtab->globalObjectList, name);
+}
+
+void checkFreshIdent(char *name) {
+  // TODO
+}
+
+Object* checkDeclaredIdent(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredConstant(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredType(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredVariable(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredFunction(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredProcedure(char* name) {
+  // TODO
+}
+
+Object* checkDeclaredLValueIdent(char* name) {
+  // TODO
+}
+
